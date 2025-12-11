deecb3a666f5600876aaf7d71e39de642386ef59
christianlarry <christianlarry03@gmail.com>
2025-12-11 19:16:39 +0800

    Add prettierrc and vscode settings

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -1,30 +1,22 @@
-import {z} from "zod"
+import { z } from "zod"
 
 export const registerSchema = z.object({
-  body: z.object({
-    email: z.email(),
-    password: z.string().min(8),
-    fullName: z.string().min(1),
-  }),
+  email: z.email(),
+  password: z.string().min(8),
+  fullName: z.string().min(1),
 })
 
 export const loginSchema = z.object({
-  body: z.object({
-    email: z.email(),
-    password: z.string(),
-  }),
+  email: z.email(),
+  password: z.string(),
 })
 
 export const updateProfileSchema = z.object({
-  body: z.object({
-    fullName: z.string().optional(),
-    email: z.string().optional(),
-  }),
+  fullName: z.string().optional(),
+  email: z.string().optional(),
 })
 
 export const changePasswordSchema = z.object({
-  body: z.object({
-    oldPassword: z.string(),
-    newPassword: z.string().min(8),
-  }),
+  oldPassword: z.string(),
+  newPassword: z.string().min(8),
 })
